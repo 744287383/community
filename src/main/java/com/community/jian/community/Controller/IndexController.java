76c63ef76ab749dfd8949277950b3e5caa95584b
408 <744287383@qq.com>
2019-07-19 09:40:20 +0800

    09点40分，提交

--- src/main/java/com/community/jian/community/Controller/IndexController.java
+++ src/main/java/com/community/jian/community/Controller/IndexController.java
@@ -3,12 +3,14 @@ package com.community.jian.community.Controller;
 
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name","谭建");
         return "index";
     }
 }
